4af8016d5d304a80eb2855e0bffc928bdcc6bcb9
tiennh1995 <nguyen.huu.tien@framgia.com>
2016-09-16 11:46:20 +0700

    Search user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < Api::BaseController
   before_action :ensure_params_exist, except: :show
   before_action :find_object, only: [:show, :update]
   before_action :correct_user, only: :update
-  before_action :ensure_params_true, :verify_shop, only: :index
+  before_action :ensure_params_true, only: :index
 
   def show
     serializer = ActiveModelSerializers::SerializableResource.new(@user,
@@ -12,12 +12,20 @@ class Api::V1::UsersController < Api::BaseController
   end
 
   def index
-    users = User.near [params[:user][:latitude], params[:user][:longitude]],
-      params[:user][:distance]
-    users = users.shipper
+    if params[:user][:search]
+      role = if current_user.shop?
+        2
+      elsif current_user.shipper?
+        1
+      end
+      users = User.search_user role, params[:user][:search]
+    else
+      users = User.near [params[:user][:latitude], params[:user][:longitude]],
+        params[:user][:distance]
+      users = users.shipper
+    end
     users = ActiveModelSerializers::SerializableResource.new(users,
       each_serializer: UserSerializer)
-
     render json: {message: I18n.t("users.messages.get_shipper_success"),
       data: {users: users.as_json}, code: 1}, status: 200
   end
@@ -40,17 +48,15 @@ class Api::V1::UsersController < Api::BaseController
   end
 
   def ensure_params_true
-    if params[:user].nil? || params[:user][:distance].nil? ||
-      params[:user][:latitude].nil? || params[:user][:longitude].nil?
+    if params[:user].nil? || params[:user].class.to_s != "ActionController::Parameters"
+      return render json: {message: I18n.t("api.missing_params"), data: {}, code: 0},
+        status: 422
+    else
+      unless (params[:user][:distance] && params[:user][:latitude] && params[:user][:longitude]) ||
+      params[:user][:search]
       render json: {message: I18n.t("api.missing_params"), data: {}, code: 0},
         status: 422
-    end
-  end
-
-  def verify_shop
-    unless current_user.shop?
-      render json: {message: I18n.t("users.messages.not_authorize"), data: {},
-        code: 0}, status: 403
+      end
     end
   end
 end
