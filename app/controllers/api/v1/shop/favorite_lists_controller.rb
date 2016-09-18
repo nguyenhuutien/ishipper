57992171139c7c73eac33c67efd714902bede4eb
dat-hedspi <dat.hedspi@gmail.com>
2016-09-18 10:01:46 +0700

    [shop] favorite list

---
+++ app/controllers/api/v1/shop/favorite_lists_controller.rb
@@ -0,0 +1,67 @@
+class Api::V1::Shop::FavoriteListsController < Api::ShopBaseController
+  before_action :ensure_params_exist, :check_exist_favorite_list,
+    :check_user_exist, only: :create
+  before_action :load_and_check_favorite_list, :destroy
+
+  def index
+    users = current_user.favorite_list_users
+    users = ActiveModelSerializers::SerializableResource.new(users,
+      each_serializer: UserSerializer, scope: {current_user: current_user})
+    render json: {message: I18n.t("favorite_list.get_favorite_list_success"),
+      data: {users: users}, code: 1}, status: 200
+  end
+
+  def create
+    favorite_list = FavoriteList.new favorite_list_params
+    favorite_list.owner = current_user
+    if favorite_list.save
+      render json: {message: I18n.t("favorite_list.create_success"),
+        data: {favorite_list: favorite_list}, code: 1}, status: 200
+    else
+      render json: {message: I18n.t("favorite_list.create_fail"), data: {},
+        code: 0}, status: 200
+    end
+  end
+
+  def destroy
+    @favorite_list.destroy
+    render json: {message: I18n.t("favorite_list.destroy_success"), data: {},
+      code: 1}, status: 200
+  end
+
+  private
+  def favorite_list_params
+    params.require(:favorite_list).permit FavoriteList::FAVORITE_LIST_ATTRIBUTES_PARAMS
+  end
+
+  def ensure_params_exist
+    unless CheckParams.new(FavoriteList::FAVORITE_LIST_ATTRIBUTES_PARAMS,
+      params[:favorite_list]).params_exist?
+      render json: {message: I18n.t("favorite_list.missing_params"), data: {},
+        code: 0}, status: 422
+    end
+  end
+
+  def check_exist_favorite_list
+    favorite_list_user = current_user.favorite_list_users.
+      find_by id: favorite_list_params[:favorite_list_user_id]
+    if favorite_list_user
+      render json: {message: I18n.t("favorite_list.favorite_list_shop_exist"),
+        data: {}, code: 0}, status: 200
+    end
+  end
+
+  def check_user_exist
+    user = User.find_by id: favorite_list_params[:favorite_list_user_id]
+    render json: {message: I18n.t("black_list.user"), data: {}, code: 0},
+      status: 422 if user.nil? or user.shop?
+  end
+
+  def load_and_check_favorite_list
+    @favorite_list = FavoriteList.find_by_id params[:id]
+    unless @favorite_list && @favorite_list.owner == current_user
+      render json: {message: I18n.t("favorite_list.destroy_fail"), data: {},
+        code: 1}, status: 200
+    end
+  end
+end
