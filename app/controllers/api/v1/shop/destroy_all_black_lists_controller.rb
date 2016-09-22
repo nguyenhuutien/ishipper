544cab542104ba4a46c28b53bdeeb28763a2edfd
dat-hedspi <dat.hedspi@gmail.com>
2016-09-22 08:49:53 +0700

    delete_all

---
+++ app/controllers/api/v1/shop/destroy_all_black_lists_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::Shop::DestroyAllBlackListsController < Api::ShopBaseController
+  before_action :load_and_check_all_black_list
+
+  def index
+    @black_list.destroy_all
+    render json: {message: I18n.t("black_list.destroy_all_success"), data: {},
+      code: 1}, status: 200
+  end
+
+  private
+  def load_and_check_all_black_list
+    @black_list = current_user.owner_black_lists
+    if @black_list.blank?
+      render json: {message: I18n.t("black_list.destroy_all_fail"), data: {},
+        code: 1}, status: 200
+    end
+  end
+end
---
+++ app/controllers/api/v1/shop/destroy_all_favorite_lists_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::Shop::DestroyAllFavoriteListsController < Api::ShopBaseController
+  before_action :load_and_check_all_black_list
+
+  def index
+    @favorite_list.destroy_all
+    render json: {message: I18n.t("favorite_list.destroy_all_success"), data: {},
+      code: 1}, status: 200
+  end
+
+  private
+  def load_and_check_all_black_list
+    @favorite_list = current_user.owner_favorite_lists
+    if @favorite_list.blank?
+      render json: {message: I18n.t("favorite_list.destroy_all_fail"), data: {},
+        code: 1}, status: 200
+    end
+  end
+end
