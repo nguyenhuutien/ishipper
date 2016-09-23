07a791011ee6694d0b7d9291ca6208759df1f9b6
tiennh1995 <nguyen.huu.tien@framgia.com>
2016-09-23 11:17:43 +0700

    heroku

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,40 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
+require "socket"
+require "json"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module IShipper
+  @@clients = Hash.new
+
+  def send_notification client, notification
+    data = notification.to_json.to_s
+    client.puts data
+  end
+
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    include IShipper
+
+    config.after_initialize do
+      server = TCPServer.new 1234
+      notification = Notification.new owner_id: 1, recipient_id: 1, key: "Test"
+      while true
+        Thread.start(server.accept) do |client|
+          @@clients[notification.owner_id] = client
+
+          while client && !client.closed?
+            s = client.gets
+            if s && s[0,16] == "get_notification"
+              send_notification client, notification
+            end
+          end
+
+          @@clients.delete notification.owner_id
+          client.close
+        end
+      end
+    end
   end
 end
