17a8a73bcc4fb2d174aea0c3e65390b7f274d548
dat-hedspi <dat.hedspi@gmail.com>
2016-09-27 11:38:47 +0700

    fix-receive-invoice

--- app/controllers/api/v1/shop/user_invoices_controller.rb
+++ app/controllers/api/v1/shop/user_invoices_controller.rb
@@ -34,4 +34,17 @@ class Api::V1::Shop::UserInvoicesController < Api::ShopBaseController
         code: 0}, status: 200
     end
   end
+
+  def check_black_list
+    black_user = @invoice.user_invoices - @invoice.user_invoices.init -
+      @invoice.user_invoices.rejected
+    unless black_user.blank?
+      black_list = BlackList.find_by owner_id: current_user.id,
+        black_list_user_id: black_user.first.user_id
+      if black_list
+        render json: {message: I18n.t("black_list.permission_denied"), data: {},
+          code: 1}, status: 200
+      end
+    end
+  end
 end
