b436fa746bc995928d7451e2825f4ddaf761bd5f
hatd <ennino1234@gmail.com>
2016-10-28 15:52:34 +0700

    return number noti unread

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,4 +3,6 @@ class Notification < ApplicationRecord
   belongs_to :recipient, class_name: User.name
 
   enum content: [:receive, :waiting, :shipping, :shipped, :finished, :cancel]
+
+  scope :unread, -> {where read: false}
 end
