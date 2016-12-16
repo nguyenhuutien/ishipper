0f1ec2ea4ace5449940901a7ee14490a4377d5e3
hatd <ennino1234@gmail.com>
2016-12-16 11:18:03 +0700

    fix-api3

--- app/jobs/notifications/send_notification_job.rb
+++ app/jobs/notifications/send_notification_job.rb
@@ -24,7 +24,7 @@ class Notifications::SendNotificationJob < ActiveJob::Base
     end
     options = {notification: {title: I18n.t("notifications.app_name"), text: text,
       click_action: click_action}, data: {notification_id: notification.id,
-      invoice_id: invoice.id, user: owner, invoice: serializer}}
+      click_action: click_action, invoice_id: invoice.id, user: owner, invoice: serializer}}
     response = fcm.send registration_ids, options
   end
 end
