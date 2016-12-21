582b9a697940e439276ac43ce61496dd49ec0c76
hatd <ennino1234@gmail.com>
2016-12-21 20:46:37 +0700

    add/remove favorite/black list

--- app/controllers/shop/shippers_controller.rb
+++ app/controllers/shop/shippers_controller.rb
@@ -6,6 +6,21 @@ class Shop::ShippersController < Shop::ShopBaseController
     else
       current_user.list_shipper_received
     end
-    @shippers = @shippers.search_user "Shipper", params[:name_shipper] if @shippers && params[:name_shipper]
+    @shippers = @shippers.search_user "Shipper", params[:name_shipper] if params[:name_shipper]
+    @shippers = ActiveModelSerializers::SerializableResource.new @shippers,
+      each_serializer: Users::ListShipperSerializer, scope: {invoice: nil,
+      current_user: current_user}
+    @current_type = params[:type].present? ? params[:type] : Settings.shipper.types.list_shipper_received
+  end
+
+  def update
+    if params[:action_type] && current_user.send("#{params[:action_type]}", "#{params[:id]}")
+      @shippers = current_user.send params[:current_type]
+      @shippers = ActiveModelSerializers::SerializableResource.new @shippers,
+        each_serializer: Users::ListShipperSerializer, scope: {invoice: nil,
+        current_user: current_user}
+      @current_type = params[:current_type]
+    end
   end
 end
+
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -9,4 +9,23 @@ class Shop < User
       user_invoices.invoice_id = invoices.id WHERE users.id = #{self.id}"
     User.where "id IN (#{ids})"
   end
+
+  ["favorite_list", "black_list"].permutation(2).to_a.each do |arg|
+    arg.each_cons(2) do |arg1, arg2|
+      define_method("add_to_#{arg1}") do |id|
+        list2 = "#{arg2}".split('_').map(&:capitalize).join('').constantize.
+          find_by "#{arg2}_user_id": id, owner_id: self.id
+        list2.destroy if list2
+        list1 = "#{arg1}".split('_').map(&:capitalize).join('').constantize.
+          new "#{arg1}_user_id": id, owner_id: self.id
+        true if list1.save
+      end
+
+      define_method("remove_from_#{arg1}") do |id|
+        list = "#{arg1}".split('_').map(&:capitalize).join('').constantize.
+          find_by "#{arg1}_user_id": id, owner_id: self.id
+        true if list && list.destroy
+      end
+    end
+  end
 end
