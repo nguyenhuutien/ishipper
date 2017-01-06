f41320a9c20cf9959bafc1df66cfec57c8232d7e
hatd <ennino1234@gmail.com>
2017-01-06 16:44:31 +0700

    fix api 4

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,16 +3,12 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      if request.headers["HTTP_PHONE_NUMBER"].nil? && request.headers["HTTP_AUTHORIZATION"].nil?
+      if request.headers["HTTP_AUTHORIZATION"].nil?
         self.current_user = find_verified_user_web
-        logger.add_tags self.current_user.user.phone_number
       else
-        phone_number = request.headers["HTTP_PHONE_NUMBER"]
-        request.headers["HTTP_AUTHORIZATION"].slice! Settings.realtime.basic
-        authorization = request.headers["HTTP_AUTHORIZATION"]
-        self.current_user = find_verified_user phone_number, authorization
-        logger.add_tags self.current_user.user.phone_number
+        self.current_user = find_verified_user request.headers["HTTP_AUTHORIZATION"]
       end
+      logger.add_tags self.current_user.user.phone_number
       if self.current_user.user.shipper?
         serializer = ActiveModelSerializers::SerializableResource.
           new(self.current_user.user).as_json
@@ -25,27 +21,20 @@ module ApplicationCable
     end
 
     protected
-    def find_verified_user phone_number, authorization
-      user = User.find_by phone_number: phone_number
-      if user
-        if user_tokens = user.user_tokens
-          user_tokens.each do |user_token|
-            if Base64.encode64(user_token.authentication_token).gsub("\n", "") == authorization
-              user_token.update_attribute "online", true
-              return user_token
-            end
-          end
-          reject_unauthorized_connection
-        end
+
+    def find_verified_user authorization
+      if user_token = UserToken.find_by(authentication_token: authorization)
+        user_token.update_attribute "online", true unless user_token.user.online?
+        user_token
+      else
+        reject_unauthorized_connection
       end
     end
 
     def find_verified_user_web
-      if cookies[:user_token_id]
-        if user_token = UserToken.find_by_id(cookies[:user_token_id])
-          user_token.update_attribute "online", true
-          user_token
-        end
+      if cookies[:user_token_id] && user_token = UserToken.find_by_id(cookies[:user_token_id])
+        user_token.update_attribute "online", true unless user_token.user.online?
+        user_token
       else
         reject_unauthorized_connection
       end
