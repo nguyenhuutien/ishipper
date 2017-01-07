ef9528a5bd2abb35ec3d2be509d2ba9eba44d71a
dat-hedspi <dat.hedspi@gmail.com>
2017-01-07 12:19:24 +0700

    fix load notification

---
+++ app/controllers/shop/notifications_controller.rb
@@ -0,0 +1,10 @@
+class Shop::NotificationsController < Shop::ShopBaseController
+  def index
+    @notifications = current_user.passive_notifications.order_by_time.
+      page(params[:page]).per Settings.notifications_per_request
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+end
